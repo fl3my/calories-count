c3316094a9d1f11c15c9105a1e4699b08be12d5d
Ross Fleming <rossflem@icloud.com>
2022-04-25 12:28:18 +0100

    Add Identity

--- DAL/CaloriesCountContext.cs
+++ DAL/CaloriesCountContext.cs
@@ -1,4 +1,5 @@
 ﻿using CaloriesCount.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -7,10 +8,20 @@ using System.Web;
 
 namespace CaloriesCount.DAL
 {
-    public class CaloriesCountContext:DbContext
+    public class CaloriesCountContext:IdentityDbContext<ApplicationUser>
     {
         // Tell Entity framework to use the model class to represent a row in the models table
         public DbSet<Food> Foods { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        public CaloriesCountContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static CaloriesCountContext Create()
+        {
+            return new CaloriesCountContext();
+        }
     }
 }
